b43e257fd1a3dc3e782f21b69cb1f59ad6cb4bbb
Christopher Regali <admin@vdop.org>
2024-07-07 02:28:04 +0200

    Add first shot at item indexing addition for vertex_property_manager.rs

--- src/managers/vertex_property_manager.rs
+++ src/managers/vertex_property_manager.rs
@@ -1,9 +1,9 @@
 use std::io::Cursor;
 use std::u8;
 
-use indradb::util;
+use indradb::{Json, util};
 use serde_json::Value as JsonValue;
-use sled::Tree;
+use sled::{IVec, Tree};
 use uuid::Uuid;
 
 use errors::map_err;
@@ -14,11 +14,12 @@ pub type OwnedPropertyItem = ((Uuid, String), JsonValue);
 
 pub struct VertexPropertyManager<'tree> {
     pub tree: &'tree Tree,
+    pub value_index_tree: &'tree Tree,
 }
 
 impl<'tree> VertexPropertyManager<'tree> {
-    pub fn new(tree: &'tree Tree) -> Self {
-        VertexPropertyManager { tree }
+    pub fn new(tree: &'tree Tree, value_index_tree: &'tree Tree) -> Self {
+        VertexPropertyManager { tree, value_index_tree }
     }
 
     fn key(&self, vertex_id: Uuid, name: &str) -> Vec<u8> {
@@ -26,6 +27,22 @@ impl<'tree> VertexPropertyManager<'tree> {
             util::Component::Uuid(vertex_id),
             util::Component::FixedLengthString(name),
         ])
+    }
+
+    fn key_value_index(&self, vertex_id: &Uuid, value: &JsonValue, property_name: &str) -> Vec<u8> {
+        util::build(&[
+            util::Component::FixedLengthString(property_name),
+            util::Component::Json(&Json::new(value.clone())),
+            util::Component::Uuid(*vertex_id)
+        ])
+    }
+
+    fn read_key_value_index(buf: IVec) -> (String, JsonValue, Uuid) {
+        let mut cursor = Cursor::new(buf.as_ref());
+        let name = util::read_fixed_length_string(&mut cursor);
+        let value = serde_json::from_reader(cursor.clone()).unwrap();
+        let uuid = util::read_uuid(&mut cursor);
+        (name, value, uuid)
     }
 
     pub fn iterate_for_owner(&self, vertex_id: Uuid) -> indradb::Result<impl Iterator<Item=indradb::Result<OwnedPropertyItem>> + '_> {
@@ -56,11 +73,24 @@ impl<'tree> VertexPropertyManager<'tree> {
         let key = self.key(vertex_id, name);
         let value_json = serde_json::to_vec(value)?;
         map_err(self.tree.insert(key.as_slice(), value_json.as_slice()))?;
+        let value_index_key = self.key_value_index(&vertex_id, value, name);
+        map_err(self.value_index_tree.insert(value_index_key, &[]))?;
         Ok(())
     }
 
     pub fn delete(&self, vertex_id: Uuid, name: &str) -> indradb::Result<()> {
         map_err(self.tree.remove(&self.key(vertex_id, name)))?;
+        let prefix = util::build(&[util::Component::FixedLengthString(name)]);
+        let items = self.value_index_tree.scan_prefix(prefix);
+        for item in items {
+            if let Ok((key, _)) = item {
+                let (n, v, vid) = Self::read_key_value_index(key);
+                if vertex_id == vid {
+                    let key = self.key_value_index(&vid, &v, n.as_str());
+                    map_err(self.value_index_tree.remove(key))?;
+                }
+            }
+        }
         Ok(())
     }
 }
